385273e0f96fad42971476074e4394fe30d0dab1
Joas Schilling <coding@schilljs.com>
2025-12-01 12:25:29 +0100

    ci(cypress): Ignore some errors in cypress like other apps do
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,3 +8,13 @@ const url = Cypress.config('baseUrl').replace(/\/index.php\/?$/g, '')
 Cypress.env('baseUrl', url)
 
 addCommands()
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+	if (err.message.includes('ResizeObserver loop limit exceeded')
+		|| err.message.includes('ResizeObserver loop completed with undelivered notifications')) {
+		return false
+	}
+
+	// we still want to ensure there are no other unexpected
+	// errors, so we let them fail the test
+})
