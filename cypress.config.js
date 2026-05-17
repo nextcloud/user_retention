17a99e6b04aca7f22d7087a56307e63b3a52d702
Grigorii K. Shartsev <me@shgk.me>
2026-05-17 22:37:06 +0200

    chore(cypress): remove @cypress/browserify-preprocessor processing
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- cypress.config.js
+++ cypress.config.js
@@ -10,11 +10,6 @@ module.exports = defineConfig({
 	viewportWidth: 1280,
 	viewportHeight: 900,
 	e2e: {
-		setupNodeEvents(on, config) {
-			const browserify = require('@cypress/browserify-preprocessor')
-			on('file:preprocessor', browserify())
-		},
-
 		baseUrl: 'http://localhost:8081/index.php/',
 		specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
 	},
